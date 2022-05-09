920b97eccec2e26ae139544b971e71239780263c
Charles Pascoe <charles@cpascoe.co.uk>
2022-05-09 17:11:48 +0100

    serenade_client.py: remove encoding option from logging config to allow support for older versions of Python 3

--- serenade_client.py
+++ serenade_client.py
@@ -27,7 +27,6 @@ async def send(message, data):
 
 logging.basicConfig(
     filename=expanduser('~/.serenade.log'),
-    encoding='utf-8',
     level=logging.INFO,
     format='%(asctime)s %(process)d %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
